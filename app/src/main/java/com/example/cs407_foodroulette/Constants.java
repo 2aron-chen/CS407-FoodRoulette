14898e9f6bfc05b82bb7315623bde87d88f5cab5
Ethan Lim <elim27@wisc.edu>
2021-11-10 08:51:14 -0600

    reordered view Constants so Home would be default

--- app/src/main/java/com/example/cs407_foodroulette/Constants.java
+++ app/src/main/java/com/example/cs407_foodroulette/Constants.java
@@ -1,9 +1,9 @@
 package com.example.cs407_foodroulette;
 
 public class Constants {
-    public static final int NEARBY = 0;
-    public static final int RECENTS = 1;
-    public static final int HOME = 2;
+    public static final int HOME = 0;
+    public static final int NEARBY = 1;
+    public static final int RECENTS = 2;
 
     public static final String PACKAGE_NAME = "com.cs407_foodroulette";
     public static final String VIEW = "VIEW";
