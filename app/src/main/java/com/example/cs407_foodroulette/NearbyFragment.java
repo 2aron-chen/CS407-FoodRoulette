78c787bc0264aad4362181091e280948f5426d98
Aaron <kchen339@wisc.edu>
2021-11-16 08:55:05 -0600

    Implement Google Maps(not finish).

--- app/src/main/java/com/example/cs407_foodroulette/NearbyFragment.java
+++ app/src/main/java/com/example/cs407_foodroulette/NearbyFragment.java
@@ -11,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class NearbyFragment extends Fragment {
 
     public NearbyFragment() {
@@ -20,6 +22,9 @@ public class NearbyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
 
         Activity activity = this.getActivity();
         SharedPreferences sharedPreferences = activity.getSharedPreferences(Constants.PACKAGE_NAME, Context.MODE_PRIVATE);
