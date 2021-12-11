fe82df5ebc454a398b62dd6cff871641a99b3e5d
Ethan Lim <elim27@wisc.edu>
2021-12-11 10:16:20 -0600

    added constants for pref defaults and keys

--- app/src/main/java/com/example/cs407_foodroulette/Constants.java
+++ app/src/main/java/com/example/cs407_foodroulette/Constants.java
@@ -8,4 +8,12 @@ public class Constants {
     public static final String PACKAGE_NAME = "com.cs407_foodroulette";
     public static final String VIEW = "VIEW";
 
+    public static final int DEFAULT_PRICE = -1;
+    public static final String DEFAULT_CUISINE = "Unknown";
+    public static final String DEFAULT_DISTANCE = "-1 Mile";
+
+    public static final String CUISINE_KEY = "CUISINE_KEY";
+    public static final String DISTANCE_KEY = "DISTANCE_KEY";
+    public static final String PRICE_KEY = "PRICE_KEY";
+
 }
