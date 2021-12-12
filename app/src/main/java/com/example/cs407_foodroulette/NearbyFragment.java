57adea98cc52153f2c1d3486eecf26e40b7112f0
Ethan Lim <elim27@wisc.edu>
2021-12-12 14:37:45 -0600

    implemented user marker, restaurant markers, and logic for removing/adding markers from map

--- app/src/main/java/com/example/cs407_foodroulette/NearbyFragment.java
+++ app/src/main/java/com/example/cs407_foodroulette/NearbyFragment.java
@@ -10,23 +10,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.cs407_foodroulette.RestuarantUtilities.Restaurant;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.MissingResourceException;
 
 public class NearbyFragment extends Fragment {
     private LatLng currLatLng;
+    private GoogleMap googleMap;
+    private List<Restaurant> restaurantList;
+    private List<Marker> markerList;
+    private int lessIdx;
+    private int moreIdx;
 
     public NearbyFragment() {
-        // Required empty public constructor
+        lessIdx = 0;
+        moreIdx = 0;
+        markerList = new ArrayList<>();
     }
 
     @Override
@@ -34,6 +48,7 @@ public class NearbyFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         Bundle bundle = this.getArguments();
+        View view = inflater.inflate(R.layout.fragment_nearby, container, false);
 
         if (bundle == null) {
             throw new MissingResourceException("ERROR: Bundle missing!", "Bundle", "");
@@ -41,7 +56,114 @@ public class NearbyFragment extends Fragment {
 
         currLatLng = new LatLng(bundle.getDouble(Constants.LAT_KEY), bundle.getDouble(Constants.LONG_KEY));
 
+        List<Restaurant> restaurantList = findRestaurants();
+        addMarkers(restaurantList);
+        moveCamera();
+        setOnClickListeners(view);
 
-        return inflater.inflate(R.layout.fragment_nearby, container, false);
+        onClickMore();
+
+        return view;
+    }
+
+    private List<Restaurant> findRestaurants() {
+        ArrayList<Restaurant> restaurantList = Restaurant.getRestaurantsByCuisinePrice(Constants.DEFAULT_CUISINE, Constants.DEFAULT_PRICE+1);
+
+        return Restaurant.getRestaurantsWithinDistance(restaurantList, 1, currLatLng.latitude, currLatLng.longitude);
+    }
+
+    private void addMarkers(List<Restaurant> restaurantList) {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(map -> {
+            googleMap = map;
+            Marker userMarker = map.addMarker(new MarkerOptions().position(currLatLng).title("You are here"));
+            userMarker.showInfoWindow();
+
+            for (Restaurant restaurant : restaurantList) {
+                Marker marker = map.addMarker(new MarkerOptions()
+                        .position(new LatLng(restaurant.getLat(), restaurant.getLng()))
+                        .title(restaurant.getRestaurantName()));
+
+                markerList.add(marker);
+            }
+
+            for (Marker marker : markerList) {
+                marker.setVisible(false);
+            }
+
+            for (int i = 0; i < markerList.size() / 6; i++) {
+                markerList.get(moreIdx).setVisible(true);
+                moreIdx++;
+            }
+
+        });
+    }
+
+    private void moveCamera() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(map -> {
+            googleMap = map;
+
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(currLatLng, 13));
+        });
+    }
+
+    private void onClickMore() {
+        int diff = markerList.size() / 6;
+
+        if (markerList.size() == 0 || (moreIdx - lessIdx) >= (markerList.size() - diff)) return;
+
+        while (diff > 0) {
+            int idx = moreIdx % markerList.size();
+            Marker marker  = markerList.get(idx);
+            moreIdx++;
+
+            if (marker.isVisible()) continue;
+
+            marker.setVisible(true);
+            diff--;
+        }
+
+    }
+
+    private void onClickLess() {
+        int diff = markerList.size() / 6;
+
+        if (markerList.size() == 0 || (lessIdx == moreIdx)) return;
+
+        while (diff > 0) {
+            int idx = lessIdx % markerList.size();
+            Marker marker  = markerList.get(idx);
+            lessIdx++;
+
+            if (!marker.isVisible()) continue;
+
+            marker.setVisible(false);
+            diff--;
+        }
+    }
+
+    private void setOnClickListeners(View view) {
+        Button button = (Button) view.findViewById(R.id.moreBtn);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                onClickMore();
+            }
+        });
+
+        button = (Button) view.findViewById(R.id.lessBtn);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                onClickLess();
+            }
+        });
     }
 }
